6ea127bd2040ad87628da706355a37708f8d31d5
Omar Shawky Anwer <omarshawkyanwer@gmail.com>
2018-11-04 07:34:43 +0200

    Add-Template-files

---
+++ HTTP_Handler.h
@@ -0,0 +1,33 @@
+//
+//  HTTP_Handler.h
+//  HTTPServer
+//
+//  Created by mac on 11/3/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#ifndef HTTP_Handler_h
+#define HTTP_Handler_h
+#include <ctime>
+#include <string>
+#include <vector>
+using namespace std;
+
+class HTTP_Handler{
+private:
+    Port client_Port;
+    IP_Address client_IP_Address;
+public:
+    HTTP_Handler(Port p,IP_Address ip);
+    time_t get_Last_Request();
+    void shutdown();
+protected:
+    bool validate_Request();
+    vector<string> parse_Request(string request);
+    //at least aquire lock on a certin resource
+    string get_Resource(string resource_Name);
+    string get_HTTP_Response();
+    void send_Response(string response);
+};
+
+#endif /* HTTP_Handler_h */
---
+++ HTTP_Server.h
@@ -0,0 +1,29 @@
+//
+//  HTTP_Server.h
+//  HTTPServer
+//
+//  Created by mac on 11/3/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#ifndef HTTP_Server_h
+#define HTTP_Server_h
+#include <string>
+using namespace std;
+
+
+class HTTP_Server{
+public:
+    HTTP_Server();
+    //initialize a connection with it's socket
+    bool initialize_Conn(IP_Address a,Port p);
+    //start listenning on port p
+    void run_Conn(Port p);
+    //to exit the HTTP server and make it down
+    void shutDown();
+protected:
+    void handle_Request(string req);
+};
+
+
+#endif /* HTTP_Server_h */
