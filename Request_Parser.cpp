71bef54a64f2a10d203a3e143314cf3b1878d158
Omar Shawky Anwer <omarshawkyanwer@gmail.com>
2018-11-07 19:22:49 +0200

    add-parsing-request

--- Request_Parser.cpp
+++ Request_Parser.cpp
@@ -10,11 +10,23 @@
 #include "Request_Parser.h"
 #include <vector>
 #include <string>
+#include <sstream>
 using namespace std;
 
 //default constructor
 Request_Parser::Request_Parser(){}
 
 vector<string> Request_Parser::parse_Request(string req){
-    return vector<string>();
+    vector<string> result;
+    string first_Line;
+    istringstream request(req);
+    while (getline(request, first_Line)) {
+        istringstream first_Line_Stream(first_Line);
+        string first_Line_field;
+        while (getline(first_Line_Stream,first_Line_field,' ')) {
+            result.push_back(first_Line_field);
+        }
+        break;
+    }
+    return result;
 }
